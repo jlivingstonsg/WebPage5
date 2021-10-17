553ecdf617f38ba2055b7b4f0f05392d44141c58
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-10-17 14:24:54 +0200

    Add project files.

---
+++ WebPage5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPage5.MvcApplication" Language="C#" %>
---
+++ WebPage5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPage5.Startup))]
+namespace WebPage5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
